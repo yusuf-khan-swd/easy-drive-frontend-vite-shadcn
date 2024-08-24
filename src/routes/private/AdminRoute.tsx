207e02d596ac5829b2356c88f6d3276c0fd69828
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-08-24 22:31:31 +0600

    Initialize AdminRoute component

---
+++ src/routes/private/AdminRoute.tsx
@@ -0,0 +1,26 @@
+import LoadingSpinner from "@/components/easy-drive/LoadingSpinner";
+import { Navigate, useLocation } from "react-router-dom";
+
+const AdminRoute = ({ children }: { children: React.ReactNode }) => {
+  const { role, loading } = { role: "user", loading: false };
+
+  const location = useLocation();
+
+  if (loading) {
+    return <LoadingSpinner />;
+  }
+
+  if (role === "admin") {
+    return children;
+  }
+
+  return (
+    <Navigate
+      to={role ? "/dashboard" : "/login"}
+      state={{ from: location }}
+      replace
+    />
+  );
+};
+
+export default AdminRoute;
