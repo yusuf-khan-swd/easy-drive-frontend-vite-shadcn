926079611dc5f67a9c1f02364b04116663371d7e
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-08-24 22:48:20 +0600

    add RouterProvider from react-router-dom to main page

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { RouterProvider } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 import { store } from "./redux/store.ts";
+import { router } from "./routes/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
+      <RouterProvider router={router} />
       <App />
     </Provider>
   </StrictMode>
