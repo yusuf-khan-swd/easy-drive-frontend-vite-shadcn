dbafd456f09c17421057d64b548ffee7d44a842a
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-08-24 21:51:14 +0600

    Rename ErrorPage component name

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,4 +1,4 @@
-const Error = () => {
+const ErrorPage = () => {
   return (
     <div>
       <h1>Error Page</h1>
@@ -6,4 +6,4 @@ const Error = () => {
   );
 };
 
-export default Error;
+export default ErrorPage;
