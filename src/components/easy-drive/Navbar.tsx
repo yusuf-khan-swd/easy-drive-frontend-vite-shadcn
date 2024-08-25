008dabbcdea5c997af7b8bea1ff383e232dc3230
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-08-25 18:40:44 +0600

    use variable to store menuItems and login items for navbar

--- src/components/easy-drive/Navbar.tsx
+++ src/components/easy-drive/Navbar.tsx
@@ -101,6 +101,86 @@ const Navbar = () => {
   const websiteName = "EasyDrive";
   const [isOpen, setIsOpen] = useState(false);
 
+  const menuItems = (
+    <>
+      <NavLink
+        to="/home"
+        className={({ isActive }) =>
+          isActive
+            ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+            : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
+        }
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/cars"
+        className={({ isActive }) =>
+          isActive
+            ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+            : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
+        }
+      >
+        Cars
+      </NavLink>
+      <NavLink
+        to="/dashboard/booking"
+        className={({ isActive }) =>
+          isActive
+            ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+            : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
+        }
+      >
+        Booking
+      </NavLink>
+      <NavLink
+        to="/about"
+        className={({ isActive }) =>
+          isActive
+            ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+            : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
+        }
+      >
+        About Us
+      </NavLink>
+      <NavLink
+        to="/contact"
+        className={({ isActive }) =>
+          isActive
+            ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+            : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
+        }
+      >
+        Contact Us
+      </NavLink>
+    </>
+  );
+
+  const loginItems = (
+    <>
+      <NavLink
+        to="/login"
+        className={({ isActive }) =>
+          isActive
+            ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+            : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
+        }
+      >
+        <Button>Login</Button>
+      </NavLink>
+      <NavLink
+        to="/register"
+        className={({ isActive }) =>
+          isActive
+            ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+            : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
+        }
+      >
+        <Button>Register</Button>
+      </NavLink>
+    </>
+  );
+
   return (
     <nav className="bg-white shadow-sm border-b border-gray-200">
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
@@ -110,80 +190,8 @@ const Navbar = () => {
               {websiteName}
             </NavLink>
           </div>
-          <div className="hidden md:flex md:space-x-4">
-            <NavLink
-              to="/home"
-              className={({ isActive }) =>
-                isActive
-                  ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
-                  : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
-              }
-            >
-              Home
-            </NavLink>
-            <NavLink
-              to="/cars"
-              className={({ isActive }) =>
-                isActive
-                  ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
-                  : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
-              }
-            >
-              Cars
-            </NavLink>
-            <NavLink
-              to="/dashboard/booking"
-              className={({ isActive }) =>
-                isActive
-                  ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
-                  : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
-              }
-            >
-              Booking
-            </NavLink>
-            <NavLink
-              to="/about"
-              className={({ isActive }) =>
-                isActive
-                  ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
-                  : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
-              }
-            >
-              About Us
-            </NavLink>
-            <NavLink
-              to="/contact"
-              className={({ isActive }) =>
-                isActive
-                  ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
-                  : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
-              }
-            >
-              Contact Us
-            </NavLink>
-          </div>
-          <div className="hidden md:flex md:space-x-4">
-            <NavLink
-              to="/login"
-              className={({ isActive }) =>
-                isActive
-                  ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
-                  : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
-              }
-            >
-              <Button>Login</Button>
-            </NavLink>
-            <NavLink
-              to="/register"
-              className={({ isActive }) =>
-                isActive
-                  ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
-                  : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
-              }
-            >
-              <Button>Register</Button>
-            </NavLink>
-          </div>
+          <div className="hidden md:flex md:space-x-4">{menuItems}</div>
+          <div className="hidden md:flex md:space-x-4">{loginItems}</div>
           <div className="-mr-2 flex md:hidden">
             <button
               onClick={() => setIsOpen(!isOpen)}
