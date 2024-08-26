5570f12d57b9f12b00e820dc71fe583a7a519714
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-08-26 13:03:46 +0600

    [done] page not found error page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,9 +1,46 @@
-const ErrorPage = () => {
+import notFoundImage from "@/assets/not_found-image.png";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
+
+const NotFoundPage = () => {
   return (
-    <div>
-      <h1>Error Page</h1>
+    <div style={{ margin: "20px 4px" }}>
+      <div
+        style={{
+          textAlign: "center",
+          display: "grid",
+          gap: "6px",
+        }}
+      >
+        <h3 style={{ fontSize: "28px", color: "red" }}>Page Not found!</h3>
+        <p>
+          <Link to="/">
+            <Button variant="outline">Return to home</Button>
+          </Link>
+        </p>
+      </div>
+      <div
+        style={{
+          margin: "8px auto",
+          maxWidth: "550px",
+          maxHeight: "550px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <img
+          src={notFoundImage}
+          style={{
+            objectFit: "contain",
+            width: "100%",
+            height: "100%",
+          }}
+          alt="Page Not Found"
+        />
+      </div>
     </div>
   );
 };
 
-export default ErrorPage;
+export default NotFoundPage;
