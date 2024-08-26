c30e7e4cf86708bcba6b175d97a64d0ad79191f7
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-08-26 22:52:15 +0600

    In cars page use get cars hook from rtk quern and if loading show loading spinner

--- src/pages/Cars.tsx
+++ src/pages/Cars.tsx
@@ -1,4 +1,14 @@
+import LoadingSpinner from "@/components/easy-drive/LoadingSpinner";
+import { useGetAllCarsQuery } from "@/redux/api/carApi";
+
 const Cars = () => {
+  const { data, isLoading } = useGetAllCarsQuery(undefined);
+  const cars = data.data;
+
+  console.log(cars);
+
+  if (isLoading) return <LoadingSpinner />;
+
   return (
     <div>
       <h1>Cars Page</h1>
