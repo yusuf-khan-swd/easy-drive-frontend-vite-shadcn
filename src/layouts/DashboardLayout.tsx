419d076ca52abdd3a46b42067d0253bbda64f634
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-08-26 20:06:51 +0600

    Add drawer with dashboard links

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,12 +1,109 @@
 import Footer from "@/components/easy-drive/Footer";
 import Navbar from "@/components/easy-drive/Navbar";
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
+
+import { Button } from "@/components/ui/button";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
 
 const DashboardLayout = () => {
   return (
     <div>
       <Navbar />
-      <h1>Dashboard Layout</h1>
+      <Drawer>
+        <DrawerTrigger asChild>
+          <Button variant="outline">Open Drawer</Button>
+        </DrawerTrigger>
+        <DrawerContent>
+          <div className="mx-auto w-full max-w-sm">
+            <div className="flex flex-col">
+              <NavLink
+                to="/dashboard/user/my-booking"
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+                    : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
+                }
+              >
+                My-Booking
+              </NavLink>
+              <NavLink
+                to="/dashboard/booking"
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+                    : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
+                }
+              >
+                Booking
+              </NavLink>
+              <NavLink
+                to="/dashboard/admin/manage-cars"
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+                    : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
+                }
+              >
+                Manage Cars
+              </NavLink>
+              <NavLink
+                to="/dashboard/admin/manage-bookings"
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+                    : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
+                }
+              >
+                Manage Bookings
+              </NavLink>
+              <NavLink
+                to="/dashboard/admin/manage-return-cars"
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+                    : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
+                }
+              >
+                Manage Return Cars
+              </NavLink>
+              <NavLink
+                to="/dashboard/admin/manage-users"
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+                    : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
+                }
+              >
+                Manage Return Cars
+              </NavLink>
+            </div>
+            <DrawerHeader>
+              <DrawerTitle>Move Goal</DrawerTitle>
+              <DrawerDescription>
+                Set your daily activity goal.
+              </DrawerDescription>
+            </DrawerHeader>
+            <div className="p-4 pb-0">
+              <div className="mt-3 h-[120px]"></div>
+            </div>
+            <DrawerFooter>
+              <DrawerClose asChild>
+                <Button variant="outline">Cancel</Button>
+              </DrawerClose>
+            </DrawerFooter>
+          </div>
+        </DrawerContent>
+      </Drawer>
+
       <Outlet />
       <Footer />
     </div>
