627d5b2b9cd965711149cdcda8e2909f3131b78c
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-08-26 20:16:10 +0600

    Update drawer items

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -24,76 +24,65 @@ const DashboardLayout = () => {
         </DrawerTrigger>
         <DrawerContent>
           <div className="mx-auto w-full max-w-sm">
-            <div className="flex flex-col">
-              <NavLink
-                to="/dashboard/user/my-booking"
-                className={({ isActive }) =>
-                  isActive
-                    ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
-                    : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
-                }
-              >
-                My-Booking
-              </NavLink>
-              <NavLink
-                to="/dashboard/booking"
-                className={({ isActive }) =>
-                  isActive
-                    ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
-                    : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
-                }
-              >
-                Booking
-              </NavLink>
-              <NavLink
-                to="/dashboard/admin/manage-cars"
-                className={({ isActive }) =>
-                  isActive
-                    ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
-                    : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
-                }
-              >
-                Manage Cars
-              </NavLink>
-              <NavLink
-                to="/dashboard/admin/manage-bookings"
-                className={({ isActive }) =>
-                  isActive
-                    ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
-                    : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
-                }
-              >
-                Manage Bookings
-              </NavLink>
-              <NavLink
-                to="/dashboard/admin/manage-return-cars"
-                className={({ isActive }) =>
-                  isActive
-                    ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
-                    : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
-                }
-              >
-                Manage Return Cars
-              </NavLink>
-              <NavLink
-                to="/dashboard/admin/manage-users"
-                className={({ isActive }) =>
-                  isActive
-                    ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
-                    : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
-                }
-              >
-                Manage Return Cars
-              </NavLink>
-            </div>
             <DrawerHeader>
-              <DrawerTitle>Move Goal</DrawerTitle>
-              <DrawerDescription>
-                Set your daily activity goal.
-              </DrawerDescription>
+              <DrawerTitle>Dashboard Menu</DrawerTitle>
+              <DrawerDescription>Click to change pages</DrawerDescription>
             </DrawerHeader>
             <div className="p-4 pb-0">
-              <div className="mt-3 h-[120px]"></div>
+              <div className="mt-3 h-[120px]">
+                <div className="flex flex-col">
+                  <NavLink
+                    to="/dashboard/user/my-booking"
+                    className={({ isActive }) =>
+                      isActive
+                        ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+                        : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
+                    }
+                  >
+                    My-Booking
+                  </NavLink>
+                  <NavLink
+                    to="/dashboard/admin/manage-cars"
+                    className={({ isActive }) =>
+                      isActive
+                        ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+                        : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
+                    }
+                  >
+                    Manage Cars
+                  </NavLink>
+                  <NavLink
+                    to="/dashboard/admin/manage-bookings"
+                    className={({ isActive }) =>
+                      isActive
+                        ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+                        : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
+                    }
+                  >
+                    Manage Bookings
+                  </NavLink>
+                  <NavLink
+                    to="/dashboard/admin/manage-return-cars"
+                    className={({ isActive }) =>
+                      isActive
+                        ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+                        : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
+                    }
+                  >
+                    Manage Return Cars
+                  </NavLink>
+                  <NavLink
+                    to="/dashboard/admin/manage-users"
+                    className={({ isActive }) =>
+                      isActive
+                        ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+                        : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
+                    }
+                  >
+                    Manage Return Cars
+                  </NavLink>
+                </div>
+              </div>
             </div>
             <DrawerFooter>
               <DrawerClose asChild>
