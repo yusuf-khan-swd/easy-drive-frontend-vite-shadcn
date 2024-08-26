e63b79258c80d0b91824cbbd5703aec758cb651a
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-08-26 13:05:22 +0600

    empty App.tsx file return only react fragment

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,5 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Button } from "./components/ui/button";
-
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-
 function App() {
-  return (
-    <div>
-      <h1 className="text-5xl font-bold">New Hello to test vercel deploy</h1>
-      <Button>Click me</Button>
-
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-
-      <Card>
-        <CardHeader>
-          <CardTitle>Card Title</CardTitle>
-          <CardDescription>Card Description</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p>Card Content</p>
-        </CardContent>
-        <CardFooter>
-          <p>Card Footer</p>
-        </CardFooter>
-      </Card>
-
-      <Select>
-        <SelectTrigger className="w-[180px]">
-          <SelectValue placeholder="Theme" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectItem value="light">Light</SelectItem>
-          <SelectItem value="dark">Dark</SelectItem>
-          <SelectItem value="system">System</SelectItem>
-        </SelectContent>
-      </Select>
-    </div>
-  );
+  return <></>;
 }
 
 export default App;
