739dc9dfb890975e9208882706fedbdb9102e40d
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-08-27 18:55:09 +0600

    use redux stored token for checking user login

--- src/routes/private/PrivateRoute.tsx
+++ src/routes/private/PrivateRoute.tsx
@@ -1,16 +1,11 @@
-import LoadingSpinner from "@/components/easy-drive/LoadingSpinner";
+import { useAppSelector } from "@/redux/hooks";
 import React from "react";
 import { Navigate } from "react-router-dom";
 
 const PrivateRoute = ({ children }: { children: React.ReactNode }) => {
-  const { userEmail, loading } = {
-    userEmail: "user@easydrive.com",
-    loading: false,
-  };
+  const token = useAppSelector((state) => state.auth.token);
 
-  if (loading) return <LoadingSpinner />;
-
-  if (userEmail) return children;
+  if (token) return children;
 
   return <Navigate to="/login" state={{ from: location }} replace />;
 };
