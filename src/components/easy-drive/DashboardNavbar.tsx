5e9e3ce4239510755479ad8a7c98cec6bccc9fc4
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-08-27 13:25:58 +0600

    move dashboard drawer to dashboard navbar

--- src/components/easy-drive/DashboardNavbar.tsx
+++ src/components/easy-drive/DashboardNavbar.tsx
@@ -5,6 +5,16 @@ import { Button } from "../ui/button";
 
 import logo from "@/assets/logo.png";
 import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuGroup,
@@ -196,6 +206,96 @@ const DashboardNavbar = () => {
                 {loginItems}
               </DropdownMenuContent>
             </DropdownMenu>
+            <Drawer>
+              <DrawerTrigger asChild>
+                <Button variant="outline">
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                </Button>
+              </DrawerTrigger>
+              <DrawerContent>
+                <div className="mx-auto w-full max-w-sm">
+                  <DrawerHeader>
+                    <DrawerTitle>Dashboard Menu</DrawerTitle>
+                    <DrawerDescription>Click to change pages</DrawerDescription>
+                  </DrawerHeader>
+                  <div className="p-4 pb-0">
+                    <div className="mt-3 h-[120px]">
+                      <div className="flex flex-col">
+                        <NavLink
+                          to="/dashboard/user/my-booking"
+                          className={({ isActive }) =>
+                            isActive
+                              ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+                              : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
+                          }
+                        >
+                          My-Booking
+                        </NavLink>
+                        <NavLink
+                          to="/dashboard/admin/manage-cars"
+                          className={({ isActive }) =>
+                            isActive
+                              ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+                              : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
+                          }
+                        >
+                          Manage Cars
+                        </NavLink>
+                        <NavLink
+                          to="/dashboard/admin/manage-bookings"
+                          className={({ isActive }) =>
+                            isActive
+                              ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+                              : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
+                          }
+                        >
+                          Manage Bookings
+                        </NavLink>
+                        <NavLink
+                          to="/dashboard/admin/manage-return-cars"
+                          className={({ isActive }) =>
+                            isActive
+                              ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+                              : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
+                          }
+                        >
+                          Manage Return Cars
+                        </NavLink>
+                        <NavLink
+                          to="/dashboard/admin/manage-users"
+                          className={({ isActive }) =>
+                            isActive
+                              ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+                              : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
+                          }
+                        >
+                          Manage Return Cars
+                        </NavLink>
+                      </div>
+                    </div>
+                  </div>
+                  <DrawerFooter>
+                    <DrawerClose asChild>
+                      <Button variant="outline">Cancel</Button>
+                    </DrawerClose>
+                  </DrawerFooter>
+                </div>
+              </DrawerContent>
+            </Drawer>
           </div>
         </div>
       </div>
--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,97 +1,12 @@
 import Footer from "@/components/easy-drive/Footer";
-import { NavLink, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 import DashboardNavbar from "@/components/easy-drive/DashboardNavbar";
-import { Button } from "@/components/ui/button";
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from "@/components/ui/drawer";
 
 const DashboardLayout = () => {
   return (
     <div>
       <DashboardNavbar />
-      <Drawer>
-        <DrawerTrigger asChild>
-          <Button variant="outline">Open Dashboard Menu</Button>
-        </DrawerTrigger>
-        <DrawerContent>
-          <div className="mx-auto w-full max-w-sm">
-            <DrawerHeader>
-              <DrawerTitle>Dashboard Menu</DrawerTitle>
-              <DrawerDescription>Click to change pages</DrawerDescription>
-            </DrawerHeader>
-            <div className="p-4 pb-0">
-              <div className="mt-3 h-[120px]">
-                <div className="flex flex-col">
-                  <NavLink
-                    to="/dashboard/user/my-booking"
-                    className={({ isActive }) =>
-                      isActive
-                        ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
-                        : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
-                    }
-                  >
-                    My-Booking
-                  </NavLink>
-                  <NavLink
-                    to="/dashboard/admin/manage-cars"
-                    className={({ isActive }) =>
-                      isActive
-                        ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
-                        : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
-                    }
-                  >
-                    Manage Cars
-                  </NavLink>
-                  <NavLink
-                    to="/dashboard/admin/manage-bookings"
-                    className={({ isActive }) =>
-                      isActive
-                        ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
-                        : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
-                    }
-                  >
-                    Manage Bookings
-                  </NavLink>
-                  <NavLink
-                    to="/dashboard/admin/manage-return-cars"
-                    className={({ isActive }) =>
-                      isActive
-                        ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
-                        : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
-                    }
-                  >
-                    Manage Return Cars
-                  </NavLink>
-                  <NavLink
-                    to="/dashboard/admin/manage-users"
-                    className={({ isActive }) =>
-                      isActive
-                        ? "text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
-                        : "text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
-                    }
-                  >
-                    Manage Return Cars
-                  </NavLink>
-                </div>
-              </div>
-            </div>
-            <DrawerFooter>
-              <DrawerClose asChild>
-                <Button variant="outline">Cancel</Button>
-              </DrawerClose>
-            </DrawerFooter>
-          </div>
-        </DrawerContent>
-      </Drawer>
 
       <main className="container mx-auto p-4 min-h-screen">
         <Outlet />
