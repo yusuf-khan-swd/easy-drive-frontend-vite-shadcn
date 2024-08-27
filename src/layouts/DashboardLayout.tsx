a02d7bf31e44835cb4ebfee4079df61f1a85bcf0
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-08-27 18:48:57 +0600

    use negative margin to offset padding of main container

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -11,10 +11,10 @@ const DashboardLayout = () => {
 
       <main className="container mx-auto p-4 min-h-screen">
         <div className="grid grid-cols-12">
-          <div className="hidden lg:block lg:col-span-2 border">
+          <div className="hidden lg:block lg:col-span-2 lg:ml-[-12px] lg:mt-[-12px]">
             <div className="flex flex-col">{dashboardItems("admin")}</div>
           </div>
-          <div className="col-span-12 lg:col-span-10 border">
+          <div className="col-span-12 lg:col-span-10">
             <Outlet />
           </div>
         </div>
