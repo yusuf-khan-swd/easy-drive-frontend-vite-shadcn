8b8b1d9e0898c96b79e0286dbd269161a4e9ebaa
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-08-27 18:46:30 +0600

    add sidebar for dashboard layout with grid

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -2,6 +2,7 @@ import Footer from "@/components/easy-drive/Footer";
 import { Outlet } from "react-router-dom";
 
 import DashboardNavbar from "@/components/easy-drive/DashboardNavbar";
+import { dashboardItems } from "@/constants/dashboardItems";
 
 const DashboardLayout = () => {
   return (
@@ -9,7 +10,14 @@ const DashboardLayout = () => {
       <DashboardNavbar />
 
       <main className="container mx-auto p-4 min-h-screen">
-        <Outlet />
+        <div className="grid grid-cols-12">
+          <div className="hidden lg:block lg:col-span-2 border">
+            <div className="flex flex-col">{dashboardItems("admin")}</div>
+          </div>
+          <div className="col-span-12 lg:col-span-10 border">
+            <Outlet />
+          </div>
+        </div>
       </main>
       <Footer />
     </div>
