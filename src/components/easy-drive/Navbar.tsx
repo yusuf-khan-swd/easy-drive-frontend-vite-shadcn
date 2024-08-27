7ab61d7ce50e403b582e4088b75a2cb4c538165b
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-08-27 13:15:51 +0600

    change profile to nav

--- src/components/easy-drive/Navbar.tsx
+++ src/components/easy-drive/Navbar.tsx
@@ -97,7 +97,7 @@ const Navbar = () => {
               <Link to={"/dashboard"}>
                 <DropdownMenuItem>
                   <User className="mr-2 h-4 w-4" />
-                  <span>Profile</span>
+                  <span>Dashboard</span>
                 </DropdownMenuItem>
               </Link>
             </DropdownMenuGroup>
