9fe2056d99a288678c3483d0de0e67a6712ab4de
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-08-27 19:35:35 +0600

    disabled email input field

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,7 +1,142 @@
+import { useAppSelector } from "@/redux/hooks";
+import { ChangeEvent, FormEvent, useState } from "react";
+import toast from "react-hot-toast";
+
+interface SignUpFormData {
+  name: string;
+  email: string;
+  phone?: string;
+  address?: string;
+}
+
 const Dashboard = () => {
+  const { user } = useAppSelector((state) => state.auth);
+
+  const [formData, setFormData] = useState<SignUpFormData>({
+    name: user?.name || "",
+    email: user?.email || "",
+    phone: user?.phone || "",
+    address: user?.address || "",
+  });
+
+  const [errors, setErrors] = useState<{ [key: string]: string }>({});
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value, type, checked } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: type === "checkbox" ? checked : value,
+    }));
+  };
+
+  const validate = (): boolean => {
+    const newErrors: { [key: string]: string } = {};
+
+    if (!formData.name) newErrors.name = "Name is required.";
+    if (!formData.email) {
+      newErrors.email = "Email is required.";
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      newErrors.email = "Email format is invalid.";
+    }
+
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const handleSubmit = async (e: FormEvent) => {
+    try {
+      e.preventDefault();
+      if (validate()) {
+        console.log("Form submitted:", formData);
+
+        console.log(formData);
+      }
+    } catch (error: any) {
+      console.log("Error: ", error);
+      toast.error(error?.data?.message || "Registration failed");
+    }
+  };
+
   return (
-    <div>
-      <h1>Dashboard Page</h1>
+    <div className=" flex items-center justify-center">
+      <form
+        className="bg-white border p-8 rounded shadow-md w-full max-w-md"
+        onSubmit={handleSubmit}
+      >
+        <h2 className="text-2xl font-bold mb-6 text-center">Information</h2>
+
+        <div className="mb-4">
+          <label htmlFor="name" className="block text-gray-700">
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            className={`mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-700 focus:border-blue-700 sm:text-sm ${
+              errors.name ? "border-red-500" : "border-gray-300"
+            }`}
+          />
+          {errors.name && <p className="text-red-500 text-xs">{errors.name}</p>}
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="email" className="block text-gray-700">
+            Email Address
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            disabled
+            value={formData.email}
+            onChange={handleChange}
+            className={`mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-700 focus:border-blue-700 sm:text-sm ${
+              errors.email ? "border-red-500" : "border-gray-300"
+            }`}
+          />
+          {errors.email && (
+            <p className="text-red-500 text-xs">{errors.email}</p>
+          )}
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="phone" className="block text-gray-700">
+            Phone Number
+          </label>
+          <input
+            type="tel"
+            id="phone"
+            name="phone"
+            value={formData.phone}
+            onChange={handleChange}
+            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-700 focus:border-blue-700 sm:text-sm"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="phone" className="block text-gray-700">
+            Address
+          </label>
+          <input
+            type="text"
+            id="address"
+            name="address"
+            value={formData.address}
+            onChange={handleChange}
+            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-700 focus:border-blue-700 sm:text-sm"
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600"
+        >
+          Update
+        </button>
+      </form>
     </div>
   );
 };
