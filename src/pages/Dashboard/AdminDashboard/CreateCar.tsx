b926fdf78eb5a6add5f0497bab77bf343c8edb3a
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-08-27 21:54:27 +0600

    Update input field errors for create car

--- src/pages/Dashboard/AdminDashboard/CreateCar.tsx
+++ src/pages/Dashboard/AdminDashboard/CreateCar.tsx
@@ -35,10 +35,13 @@ const CreateCar = () => {
 
     if (!formData.name) newErrors.name = "Name is required.";
     if (!formData.description) {
-      newErrors.email = "Description is required.";
+      newErrors.description = "Description is required.";
     }
     if (!formData.color) {
-      newErrors.email = "Color is required.";
+      newErrors.color = "Color is required.";
+    }
+    if (!formData.pricePerHour) {
+      newErrors.pricePerHour = "PricePerHour is required.";
     }
 
     setErrors(newErrors);
@@ -65,7 +68,7 @@ const CreateCar = () => {
         className="bg-white border p-8 rounded shadow-md w-full max-w-md"
         onSubmit={handleSubmit}
       >
-        <h2 className="text-2xl font-bold mb-6 text-center">Information</h2>
+        <h2 className="text-2xl font-bold mb-6 text-center">Add a Car</h2>
 
         <div className="mb-4">
           <label htmlFor="name" className="block text-gray-700">
@@ -86,7 +89,7 @@ const CreateCar = () => {
 
         <div className="mb-4">
           <label htmlFor="description" className="block text-gray-700">
-            Description Address
+            Description
           </label>
           <input
             type="description"
@@ -99,14 +102,14 @@ const CreateCar = () => {
               errors.description ? "border-red-500" : "border-gray-300"
             }`}
           />
-          {errors.email && (
-            <p className="text-red-500 text-xs">{errors.email}</p>
+          {errors.description && (
+            <p className="text-red-500 text-xs">{errors.description}</p>
           )}
         </div>
 
         <div className="mb-4">
           <label htmlFor="color" className="block text-gray-700">
-            Color Number
+            Color
           </label>
           <input
             type="text"
@@ -114,8 +117,13 @@ const CreateCar = () => {
             name="color"
             value={formData.color}
             onChange={handleChange}
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-700 focus:border-blue-700 sm:text-sm"
+            className={`mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-700 focus:border-blue-700 sm:text-sm ${
+              errors.color ? "border-red-500" : "border-gray-300"
+            }`}
           />
+          {errors.color && (
+            <p className="text-red-500 text-xs">{errors.color}</p>
+          )}
         </div>
 
         <div className="mb-4">
@@ -128,15 +136,20 @@ const CreateCar = () => {
             name="pricePerHour"
             value={formData.pricePerHour}
             onChange={handleChange}
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-700 focus:border-blue-700 sm:text-sm"
+            className={`mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-700 focus:border-blue-700 sm:text-sm ${
+              errors.pricePerHour ? "border-red-500" : "border-gray-300"
+            }`}
           />
+          {errors.pricePerHour && (
+            <p className="text-red-500 text-xs">{errors.pricePerHour}</p>
+          )}
         </div>
 
         <button
           type="submit"
           className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600"
         >
-          Update
+          Add
         </button>
       </form>
     </div>
