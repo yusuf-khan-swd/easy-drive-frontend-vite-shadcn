180929f9b2a2542f5fe49ea82e93839ae3c946a4
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-08-28 12:46:07 +0600

    add style to cars heading

--- src/pages/Cars.tsx
+++ src/pages/Cars.tsx
@@ -12,7 +12,7 @@ const Cars = () => {
 
   return (
     <div>
-      <h1>Cars Page</h1>
+      <h2 className="text-2xl font-bold text-center mb-6">Our Cars</h2>
       {cars && cars.length > 0 ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
           {cars.map((car: TCar) => (
