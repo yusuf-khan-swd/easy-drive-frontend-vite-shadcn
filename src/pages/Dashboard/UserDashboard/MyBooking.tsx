c4df56bec1910a2d8b17db7f648b8172bf543256
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-08-28 12:25:08 +0600

    call my booking api

--- src/pages/Dashboard/UserDashboard/MyBooking.tsx
+++ src/pages/Dashboard/UserDashboard/MyBooking.tsx
@@ -1,4 +1,14 @@
+import LoadingSpinner from "@/components/easy-drive/LoadingSpinner";
+import { useMyBookingQuery } from "@/redux/api/bookingApi";
+
 const MyBooking = () => {
+  const { data, isLoading } = useMyBookingQuery(undefined);
+  const bookings = data?.data;
+
+  console.log(bookings);
+
+  if (isLoading) return <LoadingSpinner />;
+
   return (
     <div>
       <h1>My Booking</h1>
