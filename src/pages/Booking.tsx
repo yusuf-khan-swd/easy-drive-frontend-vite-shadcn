39bc8239475bd3ac5a4d099f3b135444527cef31
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-08-28 17:25:52 +0600

    add key props

--- src/pages/Booking.tsx
+++ src/pages/Booking.tsx
@@ -17,7 +17,7 @@ const Booking = () => {
       {cars && cars.length > 0 ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
           {cars.map((car: TCar) => (
-            <CarCard car={car} />
+            <CarCard key={car?._id} car={car} />
           ))}
         </div>
       ) : (
