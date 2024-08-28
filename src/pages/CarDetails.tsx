554c0c01fcd5ad17b079df015cc63c21020136e9
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-08-28 13:14:39 +0600

    add car details page

--- src/pages/CarDetails.tsx
+++ src/pages/CarDetails.tsx
@@ -1,7 +1,27 @@
+import CarCard from "@/components/easy-drive/CarCard";
+import LoadingSpinner from "@/components/easy-drive/LoadingSpinner";
+import { useGetSingleCarQuery } from "@/redux/api/carApi";
+import { useParams } from "react-router-dom";
+
 const CarDetails = () => {
+  const { id } = useParams();
+  const { data, isLoading } = useGetSingleCarQuery(id || "");
+  const car = data?.data;
+
+  console.log(car);
+
+  if (isLoading) return <LoadingSpinner />;
+
   return (
     <div>
-      <h1>Car Details Page</h1>
+      <h2 className="text-2xl font-bold text-center mb-6">Car Details</h2>
+      {car ? (
+        <div className="grid grid-cols-1 gap-8">
+          <CarCard car={car} />
+        </div>
+      ) : (
+        <h3 className="text-2xl font-bold text-center">No data available</h3>
+      )}
     </div>
   );
 };
