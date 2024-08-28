800ad986cf9a18b78552bdab49244a294d371630
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-08-28 21:38:52 +0600

    Add form and input validation

--- src/pages/BookingDetails.tsx
+++ src/pages/BookingDetails.tsx
@@ -1,11 +1,13 @@
 import LoadingSpinner from "@/components/easy-drive/LoadingSpinner";
 import { useGetSingleCarQuery } from "@/redux/api/carApi";
-import { useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { FormEvent, useState } from "react";
+import toast from "react-hot-toast";
+import { useParams } from "react-router-dom";
 
 const BookingDetails = () => {
   const [date, setDate] = useState("");
   const [time, setTime] = useState("");
+  const [errors, setErrors] = useState<{ [key: string]: string }>({});
 
   const { id } = useParams();
   const { data, isLoading } = useGetSingleCarQuery(id || "");
@@ -22,13 +24,78 @@ const BookingDetails = () => {
     isElectric,
     features,
     pricePerHour,
-  } = car;
+  } = car || {};
 
   console.log({ date, time });
+
+  const validate = (): boolean => {
+    const newErrors: { [key: string]: string } = {};
+
+    if (!date) newErrors.date = "Date is required.";
+    if (!time) newErrors.time = "Time is required.";
+
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const handleSubmit = async (e: FormEvent) => {
+    try {
+      e.preventDefault();
+      if (validate()) {
+        const bookingData = { cardId: _id, date, startTime: time };
+
+        console.log(bookingData);
+
+        // const result = await createCar(carData).unwrap();
+        // toast.success(result?.message || "Car Created Successfully");
+      }
+    } catch (error: any) {
+      console.log("Error: ", error);
+      toast.error(error?.data?.message || "Car create failed");
+    }
+  };
 
   return (
     <div>
       <h2 className="text-2xl font-bold text-center mb-6">Booking Details</h2>
+      {/* Form for selecting date and time */}
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white border p-8 rounded shadow-md w-full max-w-md"
+      >
+        <div className="mb-4">
+          <label htmlFor="date" className="block text-gray-700">
+            Date
+          </label>
+          <input
+            type="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+            className={`mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-700 focus:border-blue-700 sm:text-sm ${
+              errors.date ? "border-red-500" : "border-gray-300"
+            }`}
+          />
+          {errors.date && <p className="text-red-500 text-xs">{errors.date}</p>}
+        </div>
+        <div className="mb-4">
+          <label htmlFor="name" className="block text-gray-700">
+            Start Time
+          </label>
+          <input
+            type="time"
+            value={time}
+            onChange={(e) => setTime(e.target.value)}
+            className={`mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-700 focus:border-blue-700 sm:text-sm ${
+              errors.time ? "border-red-500" : "border-gray-300"
+            }`}
+          />
+          {errors.time && <p className="text-red-500 text-xs">{errors.time}</p>}
+        </div>
+        {/* Submit button */}
+        <button className="bg-blue-700 text-white px-6 py-3 rounded-md hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-700">
+          Confirm
+        </button>
+      </form>
       {car ? (
         <div className="grid grid-cols-1 gap-8">
           <div className="bg-white rounded-lg shadow-md overflow-hidden border">
@@ -71,32 +138,12 @@ const BookingDetails = () => {
                   </div>
                 )}
               </div>
-              <Link to={`/booking/${_id}`}>
-                <button className="bg-blue-700 text-white px-6 py-3 rounded-md hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-700">
-                  Confirm
-                </button>
-              </Link>
             </div>
           </div>
         </div>
       ) : (
         <h3 className="text-2xl font-bold text-center">No data available</h3>
       )}
-
-      <div className="flex flex-col items-center justify-center space-y-4 p-6">
-        <input
-          className="p-2 border border-gray-300 rounded-md"
-          type="date"
-          value={date}
-          onChange={(e) => setDate(e.target.value)}
-        />
-        <input
-          className="p-2 border border-gray-300 rounded-md"
-          type="time"
-          value={time}
-          onChange={(e) => setTime(e.target.value)}
-        />
-      </div>
     </div>
   );
 };
