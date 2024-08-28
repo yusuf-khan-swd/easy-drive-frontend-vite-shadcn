de26a0bc5e038586a4cb0691b1fe63b7ed90e834
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-08-28 16:32:01 +0600

    Show all cars in booking page

--- src/pages/Booking.tsx
+++ src/pages/Booking.tsx
@@ -1,7 +1,28 @@
+import CarCard from "@/components/easy-drive/CarCard";
+import LoadingSpinner from "@/components/easy-drive/LoadingSpinner";
+import { useGetAllCarsQuery } from "@/redux/api/carApi";
+import { TCar } from "@/types/car";
+
 const Booking = () => {
+  const { data, isLoading } = useGetAllCarsQuery(undefined);
+  const cars = data?.data;
+
+  console.log(cars);
+
+  if (isLoading) return <LoadingSpinner />;
+
   return (
     <div>
-      <h1>Booking Page</h1>
+      <h2 className="text-2xl font-bold text-center mb-6">Our Cars</h2>
+      {cars && cars.length > 0 ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+          {cars.map((car: TCar) => (
+            <CarCard car={car} />
+          ))}
+        </div>
+      ) : (
+        <h3 className="text-2xl font-bold text-center">No cars available</h3>
+      )}
     </div>
   );
 };
