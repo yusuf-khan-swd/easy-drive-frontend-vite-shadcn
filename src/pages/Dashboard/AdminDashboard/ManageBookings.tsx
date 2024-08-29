ebc3c2a184ba0b0648907be990ab369cb3e46230
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-08-29 10:53:12 +0600

    remove console log

--- src/pages/Dashboard/AdminDashboard/ManageBookings.tsx
+++ src/pages/Dashboard/AdminDashboard/ManageBookings.tsx
@@ -11,7 +11,6 @@ import toast from "react-hot-toast";
 const ManageBooking = () => {
   const { data, isLoading } = useGetAllBookingsQuery(undefined);
   const bookings = data?.data;
-  console.log(bookings);
   const [deleteBooking] = useDeleteBookingMutation();
 
   const handleDelete = async (id: string) => {
