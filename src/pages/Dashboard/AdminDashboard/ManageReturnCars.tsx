7bde0f9fb1720c4b326cd2be7c1ef456194d77d1
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-08-29 10:55:11 +0600

    init ManageReturnCars

--- src/pages/Dashboard/AdminDashboard/ManageReturnCars.tsx
+++ src/pages/Dashboard/AdminDashboard/ManageReturnCars.tsx
@@ -1,7 +1,78 @@
+import { DataTable } from "@/components/easy-drive/DataTable";
+import LoadingSpinner from "@/components/easy-drive/LoadingSpinner";
+import { Button } from "@/components/ui/button";
+import {
+  useDeleteBookingMutation,
+  useGetAllBookingsQuery,
+} from "@/redux/api/bookingApi";
+import { ColumnDef } from "@tanstack/react-table";
+import toast from "react-hot-toast";
+
 const ManageReturnCars = () => {
+  const { data, isLoading } = useGetAllBookingsQuery(undefined);
+  const bookings = data?.data;
+  const [deleteBooking] = useDeleteBookingMutation();
+
+  const handleDelete = async (id: string) => {
+    try {
+      const result = await deleteBooking(id).unwrap();
+      toast.success(result?.message || "Booking deleted Successfully");
+    } catch (error: any) {
+      console.log("Error: ", error);
+      toast.error(error?.data?.message || "Booking delete failed");
+    }
+  };
+
+  if (isLoading) return <LoadingSpinner />;
+
+  const columns: ColumnDef<any>[] = [
+    {
+      accessorKey: "user.email",
+      header: "User",
+    },
+    {
+      accessorKey: "car.name",
+      header: "Car",
+    },
+    {
+      accessorKey: "date",
+      header: "Date",
+    },
+    {
+      accessorKey: "startTime",
+      header: "StartTime",
+    },
+    {
+      accessorKey: "totalCost",
+      header: "TotalCost",
+    },
+    {
+      id: "actions",
+      cell: ({ row }) => {
+        const id = row.original._id;
+        console.log(id);
+
+        return (
+          <div className="space-x-2">
+            <Button onClick={() => handleDelete(id)}>Return</Button>
+          </div>
+        );
+      },
+    },
+  ];
+
   return (
     <div>
-      <h1>Manage Return Cars Page</h1>
+      <div>
+        <h2 className="text-2xl font-bold">Manage Return Cars</h2>
+      </div>
+      <div className="w-full py-4">
+        {!bookings || bookings?.length < 1 ? (
+          <h2>No Data Available</h2>
+        ) : (
+          <DataTable columns={columns} data={bookings} />
+        )}
+      </div>
     </div>
   );
 };
